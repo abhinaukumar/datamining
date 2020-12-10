72a614aa430f4b25ef69584fd4cf833d232bb5a8
Abhinau Kumar <ab.kumr98@gmail.com>
2020-12-10 01:10:20 -0600

    Cleaned up code

--- federated_learning.py
+++ federated_learning.py
@@ -15,18 +15,12 @@ np.random.seed(0)
 parser = argparse.ArgumentParser(description='Code to train RNN and intepretable RNN models')
 parser.add_argument('--path', help='Path to dataset', type=str, required=True)
 parser.add_argument('--epochs', help='Number of epochs for which to train the model', type=int, default=10)
-parser.add_argument('--batch_size', help='Number of examples to use per update', type=int, default=10)
 parser.add_argument('--lr', help='Learning rate', type=float, default=1e-3)
 parser.add_argument('--reverse_input', help='Flag to reverse input', action='store_true')
 parser.add_argument('--no_cuda', dest='use_cuda', help='Flag to not use CUDA', action='store_false')
 parser.set_defaults(use_cuda=True)
 args = parser.parse_args(['--path', 'mini_eicu_features.csv'])
 assert os.path.exists(args.path), 'Path to dataset does not exist'
-# widgets = [
-#             progressbar.ETA(),
-#             progressbar.Bar(),
-#             ' ', progressbar.DynamicMessage('Error')
-#             ]
 
 args.batch_size = 1
 embedding_size = 40
@@ -43,13 +37,9 @@ data_generator3 = DataGenerator(path3, args.batch_size, mode='train', use_cuda=a
 data_generator4 = DataGenerator(path4, args.batch_size, mode='train', use_cuda=args.use_cuda)
 data_generator5 = DataGenerator(path5, args.batch_size, mode='train', use_cuda=args.use_cuda)
 
-
-data_generator = [data_generator1,data_generator2, data_generator3, data_generator4,data_generator5]
+data_generator = [data_generator1, data_generator2, data_generator3, data_generator4, data_generator5]
 
 model_glob = BiLSTMModel(21, embedding_size, hidden_size)
-if args.use_cuda:
-    model_glob = model_glob.cuda()
-    model_glob.tensors_to_cuda()
 
 if args.use_cuda:
     model_glob = model_glob.cuda()
@@ -58,7 +48,7 @@ if args.use_cuda:
 model_glob.train()
 w_glob = model_glob.state_dict()
 
-# training
+# Training
 loss_train = []
 lr_base_t0 = time.time()
 print(time.localtime(lr_base_t0))
@@ -85,5 +75,4 @@ for iter in range(args.epochs):
 lr_base_t1 = time.time()
 print(time.localtime(lr_base_t1))
 save_model(model_glob, 'fedmodels', {'embedding_size': embedding_size, 'hidden_size': hidden_size, 'lr': args.lr, 'epochs':args.epochs, 'batch_size':args.batch_size, 'reversed':args.reverse_input})
-
 
